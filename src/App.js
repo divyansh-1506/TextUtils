b23c92809fded91cc96f725d2192598652900f44
divyansh-1506 <79587368+divyansh-1506@users.noreply.github.com>
2022-07-27 10:37:48 +0530

    After adding router to the app (alerts not implemented yet)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import { useState } from "react";
-import Alert from "./components/Alert";
-// import About from './components/About';
+// import Alert from "./components/Alert";
+// import About from "./components/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
 
 function App() {
   let [mode, setmode] = useState("light");
+  //   let [alert, setalert] = useState(null);
 
   const toggleMode = () => {
     if (mode === "light") {
@@ -18,13 +21,28 @@ function App() {
       document.body.style.color = "black";
       setmode("light");
     }
-  }
+  };
   return (
     <>
-      <Navbar title="TextUtils" item1="Home" item2="About Us" mode={mode} toggleMode={toggleMode}/>
-    <Alert Alert= "This is an alert" mode= {mode}/>
-      <TextForm mode = {mode}/>
-      {/* <About/> */}
+      <Router>
+        <Navbar
+          title="TextUtils"
+          item1="Home"
+          item2="About Us"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Routes>
+          <Route path="/" element={<TextForm mode={mode} />}></Route>
+
+          <Route path="/about" element={<About mode={mode}></About>}></Route>
+        </Routes>
+      </Router>
+
+      {/* <TextForm mode={mode}></TextForm> */}
+      {/* <About mode = {mode}> </About> */}
+
+      {/* <Alert Alert= "Copied to clipboard!" mode= {mode}/> */}
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 
 // Dark different tones
@@ -14,23 +15,23 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg bg-${props.mode} ${props.mode==='dark'?'navbar-dark':''}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">{props.title}</a>
+    <Link className="navbar-brand" to="/">{props.title}</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">{props.item1}</a>
+          <Link className="nav-link active" aria-current="page" to="/">{props.item1}</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">{props.item2}</a>
+          <Link className="nav-link" to="/about">{props.item2}</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">{props.item3}</a>
+          <Link className="nav-link" to="/">{props.item3}</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">{props.item4}</a>
+          <Link className="nav-link" to="/">{props.item4}</Link>
         </li>
       </ul>
       {/* <form className="d-flex" role="search">
